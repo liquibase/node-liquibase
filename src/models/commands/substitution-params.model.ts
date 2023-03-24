ab75224a1ef16572d2a3e996e7a52ddfcdbeb003
Taylor Buckner <taylor.a.buckner@gmail.com>
2023-03-24 10:30:15 -0500

    (feat): Define a generic interface for commands that allow substitution params.

---
+++ src/models/commands/substitution-params.model.ts
@@ -0,0 +1,13 @@
+export interface ParamsModel {
+  [key: string]: any;
+}
+
+export type SubstitutionParamsModel = ParamsModel;
+
+type HasSubstitutionParams = {
+  substitutionParams: SubstitutionParamsModel;
+};
+
+export type WithSubstitutionParams<T> = HasSubstitutionParams & T;
+
+export type WithOptionalSubstitutionParams<T> = Partial<HasSubstitutionParams> & T;
